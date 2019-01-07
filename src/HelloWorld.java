3877583ea4327817c3b2babe9da1026725762998
ericvasquez <eric.vasquez3327@gmail.com>
2019-01-07 13:15:21 -0600

    added popup for message

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,12 @@
+import javax.swing.JOptionPane;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        //uses two lines of code to print message on  a single line instead of one
         System.out.print("Hello, ");
         System.out.println("World!");
 
+        //prints message in popup
+        JOptionPane.showMessageDialog(null, "Hello, World!");
     }
 }
