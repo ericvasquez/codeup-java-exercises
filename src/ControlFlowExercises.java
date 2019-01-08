12932ca7149bced52ed116f8c8e4300621f79f3e
ericvasquez <eric.vasquez3327@gmail.com>
2019-01-08 12:39:18 -0600

    Changed all loops to for loops

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -4,29 +4,38 @@ import java.util.*;
 public class ControlFlowExercises {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-//
-//        for(int i = 5; i<=15; i++)
+
+
+//        int i = 5;
+//        while (i<=15){
 //            System.out.print(i + " ");
-//            System.out.println();
+//            i++;
+//        }
 
-        int i = 5;
-        while (i<=15){
+
+        for(int i = 5; i<=15; i++)
             System.out.print(i + " ");
-            i++;
-        }
-
-
+        System.out.println();
 //        int number = 2;
 //        do {
 //            System.out.println(number);
 //            number+=2;
 //        }while (number<=100);
 
+        for(int number = 2; number<=100; number+=2)
+            System.out.println(number + " ");
+
+
+
+
 //int newNumber = 100;
 //do{
 //    System.out.println(newNumber);
 //    newNumber-=5;
 //}while (newNumber>=-10);
+
+for(int newNumber = 100; newNumber>=-10; newNumber-=5)
+    System.out.println(newNumber);
 
 
 //        long numberSquared = 2;
@@ -35,7 +44,8 @@ public class ControlFlowExercises {
 //            numberSquared*=numberSquared;
 //        }while (numberSquared<1000000);
 //
-
+for(long numberSquared = 2; numberSquared<1000000; numberSquared*=numberSquared)
+    System.out.println(numberSquared);
 
     }
 }
