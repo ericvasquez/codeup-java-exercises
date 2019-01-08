1b6477f050105db369606609092d1e5c23291561
ericvasquez <eric.vasquez3327@gmail.com>
2019-01-08 12:16:17 -0600

    Part b/counted by 2's

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import javax.swing.JOptionPane;
+import java.util.*;
 
 public class ControlFlowExercises {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+//
+//        for(int i = 5; i<=15; i++)
+//            System.out.print(i + " ");
+//            System.out.println();
+
+
+        int number = 2;
+        do {
+            System.out.println(number);
+            number+=2;
+        }while (number<=100);
+
+
+
+    }
+
+
 }
