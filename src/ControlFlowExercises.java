ae5668567ffa1b20aa43f3354ef9966ac49781b4
ericvasquez <eric.vasquez3327@gmail.com>
2019-01-08 15:21:03 -0600

    Completed Display a table of powers

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -47,25 +47,53 @@ public class ControlFlowExercises {
 //for(long numberSquared = 2; numberSquared<1000000; numberSquared*=numberSquared)
 //    System.out.println(numberSquared);
 
-for(int fb = 1; fb<=100; fb++){
-    if(fb%3==0 && fb%5==0){
-        System.out.println("FizzBuzz");
-        continue;}
+//for(int fb = 1; fb<=100; fb++){
+//    if(fb%3==0 && fb%5==0){
+//        System.out.println("FizzBuzz");
+//        continue;}
+//
+//    if(fb%3==0){
+//        System.out.println("Fizz");
+//        continue;}
+//
+//    if(fb%5==0){
+//        System.out.println("Buzz");
+//        continue;}
+//
+//    else
+//        System.out.println(fb);
 
-    if(fb%3==0){
-        System.out.println("Fizz");
-        continue;}
-
-    if(fb%5==0){
-        System.out.println("Buzz");
-        continue;}
-
-    else
-        System.out.println(fb);
-
-}
+//}
 
 
+        boolean answer = true;
+        while(answer == true){
+            System.out.println("What number would you like to go up to? ");
+            int highestNumber = scan.nextInt();
+
+
+            System.out.println("Here is your table!");
+            System.out.println("Number  |  Squared  |  Cubed");
+            System.out.println("------  |  -------  |  -----");
+
+
+            for(int startingNumber = 1; startingNumber<=highestNumber; startingNumber++) {
+                int startingNumberSq = startingNumber * startingNumber;
+                int startingNumberCu = startingNumber * startingNumber * startingNumber;
+                System.out.println(startingNumber + "       |  " + startingNumberSq + "        |  " + startingNumberCu);
+
+            }
+            scan.nextLine();
+
+
+        System.out.println("Would you like to continue? Y or N?");
+        String newAnswer = scan.nextLine();
+
+        if(newAnswer.equalsIgnoreCase("Y"))
+            answer=true;
+        else
+            answer=false;
+    }
 
     }
 }
