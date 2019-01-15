eda6f705b1a85633c02b03c941db3dcc20bf5910
ericvasquez <eric.vasquez3327@gmail.com>
2019-01-15 01:50:40 -0600

    first commit

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,99 +1,98 @@
-import javax.swing.JOptionPane;
-import java.util.*;
+import java.util.Scanner;
 
 public class ControlFlowExercises {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
 
 
-//        int i = 5;
-//        while (i<=15){
-//            System.out.print(i + " ");
-//            i++;
-//        }
+        int i = 5;
+        while (i<=15){
+            System.out.print(i + " ");
+            i++;
+        }
 
 
-//        for(int i = 5; i<=15; i++)
-//            System.out.print(i + " ");
-//        System.out.println();
-////        int number = 2;
-////        do {
-////            System.out.println(number);
-////            number+=2;
-////        }while (number<=100);
-//
-//        for(int number = 2; number<=100; number+=2)
-//            System.out.println(number + " ");
-//
-//
-//
+        for(int i2 = 5; i2<=15; i2++)
+            System.out.print(i + " ");
+        System.out.println();
+        int number = 2;
+        do {
+            System.out.println(number);
+            number+=2;
+        }while (number<=100);
 
-//int newNumber = 100;
-//do{
-//    System.out.println(newNumber);
-//    newNumber-=5;
-//}while (newNumber>=-10);
-//
-//for(int newNumber = 100; newNumber>=-10; newNumber-=5)
-//    System.out.println(newNumber);
-//
-//
-////        long numberSquared = 2;
-////        do{
-////            System.out.println(numberSquared);
-////            numberSquared*=numberSquared;
-////        }while (numberSquared<1000000);
-////
-//for(long numberSquared = 2; numberSquared<1000000; numberSquared*=numberSquared)
-//    System.out.println(numberSquared);
-
-//for(int fb = 1; fb<=100; fb++){
-//    if(fb%3==0 && fb%5==0){
-//        System.out.println("FizzBuzz");
-//        continue;}
-//
-//    if(fb%3==0){
-//        System.out.println("Fizz");
-//        continue;}
-//
-//    if(fb%5==0){
-//        System.out.println("Buzz");
-//        continue;}
-//
-//    else
-//        System.out.println(fb);
-
-//}
+        for(int number2 = 2; number2<=100; number2+=2)
+            System.out.println(number2 + " ");
 
 
-//        boolean answer = true;
-//        while(answer == true){
-//            System.out.println("What number would you like to go up to? ");
-//            int highestNumber = scan.nextInt();
+
+
+int newNumber = 100;
+do{
+    System.out.println(newNumber);
+    newNumber-=5;
+}while (newNumber>=-10);
+
+for(int newNumber2 = 100; newNumber2>=-10; newNumber2-=5)
+    System.out.println(newNumber2);
+
+
+//        long numberSquared = 2;
+//        do{
+//            System.out.println(numberSquared);
+//            numberSquared*=numberSquared;
+//        }while (numberSquared<1000000);
 //
-//
-//            System.out.println("Here is your table!");
-//            System.out.println("Number  |  Squared  |  Cubed");
-//            System.out.println("------  |  -------  |  -----");
-//
-//
-//            for(int startingNumber = 1; startingNumber<=highestNumber; startingNumber++) {
-//                int startingNumberSq = startingNumber * startingNumber;
-//                int startingNumberCu = startingNumber * startingNumber * startingNumber;
-//                System.out.println(startingNumber + "       |  " + startingNumberSq + "        |  " + startingNumberCu);
-//
-//            }
-//            scan.nextLine();
-//
-//
-//        System.out.println("Would you like to continue? Y or N?");
-//        String newAnswer = scan.nextLine();
-//
-//        if(newAnswer.equalsIgnoreCase("Y"))
-//            answer=true;
-//        else
-//            answer=false;
-//    }
+for(long numberSquared = 2; numberSquared<1000000; numberSquared*=numberSquared)
+    System.out.println(numberSquared);
+
+for(int fb = 1; fb<=100; fb++){
+    if(fb%3==0 && fb%5==0){
+        System.out.println("FizzBuzz");
+        continue;}
+
+    if(fb%3==0){
+        System.out.println("Fizz");
+        continue;}
+
+    if(fb%5==0){
+        System.out.println("Buzz");
+        continue;}
+
+    else
+        System.out.println(fb);
+
+}
+
+
+        boolean answer = true;
+        while(answer == true){
+            System.out.println("What number would you like to go up to? ");
+            int highestNumber = scan.nextInt();
+
+
+            System.out.println("Here is your table!");
+            System.out.println("Number  |  Squared  |  Cubed");
+            System.out.println("------  |  -------  |  -----");
+
+
+            for(int startingNumber = 1; startingNumber<=highestNumber; startingNumber++) {
+                int startingNumberSq = startingNumber * startingNumber;
+                int startingNumberCu = startingNumber * startingNumber * startingNumber;
+                System.out.println(startingNumber + "       |  " + startingNumberSq + "        |  " + startingNumberCu);
+
+            }
+            scan.nextLine();
+
+
+        System.out.println("Would you like to continue? Y or N?");
+        String newAnswer = scan.nextLine();
+
+        if(newAnswer.equalsIgnoreCase("Y"))
+            answer=true;
+        else
+            answer=false;
+    }
 String gradeLetter = "";
         boolean gradeAnswer = true;
         while(gradeAnswer == true){
--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
+
+
 
 public class Person {
+    private String name;
+
+    public Person(String name){
+        setName(name);
+    }
+
+
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+this.name=name;
+
+    }
+
+    public void sayHello(){
+        System.out.println("Hello " + name);
+    }
+
+
+    public static void main(String[] args){
+
+Person person1 =  new Person("Some Guy");
+        System.out.println("The person's name is:" + person1.getName());
+        person1.sayHello();
+
+
+
+    }
 }
