a9b99c63ab5ec5dc4ae8849b194e74ae1ecf9eab
ericvasquez <eric.vasquez3327@gmail.com>
2019-01-15 02:10:51 -0600

    first commit

---
+++ src/StringExercise.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+
+public class StringExercise {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+
+
+
+        String message = ("We don't need no education \n"  +
+        "We don't need no thought control \n" +
+        "Check \"this\" out!, \"s inside of \"s! \n" +
+
+                "In windows, the main drive is usually C:\\  \n" +
+        "I can do backslashes \\, double backslashes \\" + "\\, \n" +
+        "and the amazing triple backslash \\\\" + "\\!" +  "\n");
+
+
+                System.out.println(message);
+
+
+
+
+
+    }
+}
